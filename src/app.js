13e61abf09bb77e2363df318cab52cb5bf223ff1
jeff-c44 <156033827+jeff-c44@users.noreply.github.com>
2024-06-28 21:33:12 +0000

    unfinished

--- src/app.js
+++ src/app.js
@@ -6,6 +6,29 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  //let submitBtn = document.getElementById("formSubmit");
+  // submitBtn.addEventListener("click", function(e) {
+  // e.preventDefault();
+  // });
+
   console.log("Hello Rigo from the console!");
 };
+let submitBtn = document.getElementById("formSubmit");
+submitBtn.addEventListener("click", checkValid);
+function checkValid() {
+  let alert = document.getElementById("blankForm");
+  let submitBtn = document.getElementById("formSubmit");
+  let card = document.querySelector("#cardNum");
+  let cvc = document.getElementById("cvcNum");
+  let amount = document.getElementById("amount");
+  let fn = document.getElementById("fn");
+  let ln = document.getElementById("ln");
+  let city = document.getElementById("city");
+  let states = document.getElementById("stateSelect");
+  let zip = document.getElementById("zipCode");
+  let message = document.getElementById("message");
+
+  if (card.value != 2) {
+    alert.classList.remove("d-none");
+  }
+}
