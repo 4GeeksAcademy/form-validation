6f1ae3854ac47ed63e56aec58996511a001260bf
jeff-c44 <156033827+jeff-c44@users.noreply.github.com>
2024-07-01 16:25:42 +0000

    Final

--- src/app.js
+++ src/app.js
@@ -4,31 +4,16 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //let submitBtn = document.getElementById("formSubmit");
-  // submitBtn.addEventListener("click", function(e) {
-  // e.preventDefault();
-  // });
-
-  console.log("Hello Rigo from the console!");
-};
 let submitBtn = document.getElementById("formSubmit");
-submitBtn.addEventListener("click", checkValid);
+window.onload = function() {
+  console.log("Hello Rigo from the console!");
+  submitBtn.addEventListener("click", checkValid);
+};
 function checkValid() {
   let alert = document.getElementById("blankForm");
-  let submitBtn = document.getElementById("formSubmit");
-  let card = document.querySelector("#cardNum");
-  let cvc = document.getElementById("cvcNum");
-  let amount = document.getElementById("amount");
-  let fn = document.getElementById("fn");
-  let ln = document.getElementById("ln");
-  let city = document.getElementById("city");
-  let states = document.getElementById("stateSelect");
-  let zip = document.getElementById("zipCode");
-  let message = document.getElementById("message");
+  let card = document.getElementById("cardNum");
 
-  if (card.value != 2) {
+  if (card.value != 16) {
     alert.classList.remove("d-none");
   }
 }
